15a22404362a6f21055cf27c2c74e9ea4e431c1a
Juliano Tavares <julianojstavares@outlook.com>
2022-04-22 10:02:06 -0300

    Clear Clients OK

--- src/modules/Clientes/useCases/clear/clearClientsUseCase.ts
+++ src/modules/Clientes/useCases/clear/clearClientsUseCase.ts
@@ -1,8 +1,19 @@
+import { prisma } from "../../../../database/prismaClient";
+
 export class ClearClientsUseCase {
 
     async execute() {
 
-        
+        await prisma.clients.deleteMany();
+
+        const nClients = await prisma.clients.count();
+
+        const clearSuccess = {
+            status: "Successfully Cleaned",
+            message: `${nClients} clientes no banco de dados`,
+          }
+      
+          return clearSuccess; 
 
     }
 
--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import { ClearClientsController } from "./modules/Clientes/useCases/clear/clearClientsController";
 import { PopulateClientsController } from "./modules/Clientes/useCases/populate/populateClientsController";
 
 const routes = Router();
 
+// Clientes
 const populateClientsController = new PopulateClientsController();
 routes.post("/clients", populateClientsController.handle);
+
+const clearClientsController = new ClearClientsController();
+routes.delete("/clients", clearClientsController.handle);
 
 export { routes };
