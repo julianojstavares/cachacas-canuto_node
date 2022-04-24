95e09be4e9ccaf206107079a7a085a3d8f789928
Juliano Tavares <julianojstavares@outlook.com>
2022-04-24 12:42:31 -0300

    Add populate test

---
+++ src/modules/Clientes/useCases/populate/populateClients.test.ts
@@ -0,0 +1,24 @@
+import { prisma } from '../../../../database/prismaClient';
+import { PopulateClientsUseCase } from "./populateClientsUseCase";
+
+describe("Populate Clients", () => {
+
+    beforeAll(async () => {
+        
+        const data = await prisma.clients.count();
+
+        if (data > 0) await prisma.clients.deleteMany({});
+
+    })
+
+    test("should return a list of clients", async () => {
+
+        const populateClientsUseCase = new PopulateClientsUseCase();
+
+        const clients = await populateClientsUseCase.execute();
+        
+        expect(clients.length).toBeGreaterThan(0);
+
+    });
+
+})
