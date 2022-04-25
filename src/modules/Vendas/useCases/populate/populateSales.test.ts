17b73d76bdb696e34fb75a6daf3a695b32c6cb36
Juliano Tavares <julianojstavares@outlook.com>
2022-04-25 19:35:20 -0300

    Populate sales ok

---
+++ src/modules/Vendas/useCases/populate/populateSales.test.ts
@@ -0,0 +1,60 @@
+import { prisma } from "../../../../database/prismaClient";
+import { PopulateClientsUseCase } from "../../../Clientes/useCases/populate/populateClientsUseCase";
+import { PopulateProductsUseCase } from "../../../Produtos/useCases/populate/populateProductsUseCase";
+import { PopulateSalesUseCase } from "./populateSalesUseCase";
+
+
+describe("Populate table sales", () => {
+    
+    jest.setTimeout(20 * 1000);
+    
+    beforeAll(async () => {
+
+        console.log("before all sales");
+        
+        const nClients = await prisma.clients.count();
+        const nProducts = await prisma.products.count();
+        
+        if(nClients < 1)
+        {
+            const populateClientsUseCase = new PopulateClientsUseCase();
+            await populateClientsUseCase.execute();
+        }
+    
+        if(nProducts < 1)
+        {
+            const populateProductsUseCase = new PopulateProductsUseCase();
+            await populateProductsUseCase.execute();
+        }
+    })
+    
+    test("should return a list of sales", async () => {
+        
+        const populateSalesUseCase = new PopulateSalesUseCase();
+        
+        const sales = await populateSalesUseCase.execute();
+
+        expect(sales.length).toBeGreaterThan(0);
+
+        const venda = await prisma.sales.findFirst({
+            include: {
+                clientRelation: true,
+                itensVendidos: true,
+            }
+        })
+        console.log(venda);
+        // expect(true).toBe(true);
+
+    });
+
+    afterAll(async () => {
+        
+        await prisma.itemsSold.deleteMany({});
+        await prisma.sales.deleteMany({});
+        await prisma.clients.deleteMany({});
+        await prisma.products.deleteMany({});
+
+    })
+
+
+})
