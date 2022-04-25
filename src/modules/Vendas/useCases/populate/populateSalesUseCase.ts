70aaad84d30fa76a02649089bafe1035dfebe230
Juliano Tavares <julianojstavares@outlook.com>
2022-04-25 20:16:31 -0300

    Update test

--- src/modules/Vendas/useCases/populate/populateSalesUseCase.ts
+++ src/modules/Vendas/useCases/populate/populateSalesUseCase.ts
@@ -2,25 +2,13 @@ import { Sales } from "@prisma/client";
 import axios from "axios";
 import { prisma } from "../../../../database/prismaClient";
 
-interface IVenda {
-    
-    Id: string;
-    IdCliente: string;
-    Data: Date;
-    Itens: [{
-        Id: string;
-        Quantidade: number;
-    }];
-
-}
-
 export class PopulateSalesUseCase {
 
     async execute() {
 
         const { data } = await axios.get('https://firebasestorage.googleapis.com/v0/b/testemonomytobackend/o/Vendas.json?alt=media&token=792a67d4-d0d0-4b9a-a099-86165322ce2a');
 
-        data.forEach(async (element:IVenda) => {
+        data.forEach(async (element:any) => {
 
             const venda = await prisma.sales.create({
 
@@ -43,6 +31,7 @@ export class PopulateSalesUseCase {
                     data: {
 
                         quantidade: item.Quantidade,
+                        precoUnitario: item.PrecoUnitario,
                         saleRelation: {
                             connect: {
                                 id: venda.id
