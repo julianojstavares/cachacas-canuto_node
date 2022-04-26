8af76fa45436c911afd72fcb4b2449daa9724225
Juliano Tavares <julianojstavares@outlook.com>
2022-04-26 17:53:32 -0300

    Updating populate sales use case

--- src/modules/Vendas/useCases/populate/populateSalesUseCase.ts
+++ src/modules/Vendas/useCases/populate/populateSalesUseCase.ts
@@ -8,49 +8,107 @@ export class PopulateSalesUseCase {
 
         // const { data } = await axios.get('https://firebasestorage.googleapis.com/v0/b/testemonomytobackend/o/Vendas.json?alt=media&token=792a67d4-d0d0-4b9a-a099-86165322ce2a');
 
-        jsonVendas.forEach(async (element:any) => {
+        async function saveData() {
+            
+            await Promise.all(jsonVendas.map(async (element) => {
 
-            const venda = await prisma.sales.create({
-
-               data: {
-
-                   data: new Date(element.Data),
-                   clientRelation: {
-                       connect: {
-                           id: element.IdCliente
-                       }
-                   },
-               }
-
-            });
-
-            element.Itens.forEach(async (item:any) => {
-
-                await prisma.itemsSold.create({
+                let venda = await prisma.sales.create({
 
                     data: {
 
-                        quantidade: item.Quantidade,
-                        precoUnitario: item.PrecoUnitario,
-                        saleRelation: {
-                            connect: {
-                                id: venda.id
-                            }
-                        },
-                        productRelation: {
-                            connect: {
-                                id: item.Id
+                        id: element.Id,
+                        data: new Date(element.Data),
+                        clientRelation: {
+                            connectOrCreate: {
+                                where: {
+                                    id: element.IdCliente
+                                },
+                                create: {
+                                    nome: "bla",
+                                    dataNascimento: new Date(Date.now()),
+                                }
                             }
                         }
-
+                        
                     }
+     
+                 });
 
-                });
+                 await Promise.all(element.Itens.map(async (item) => {
+
+                    await prisma.itemsSold.create({
+
+                        data: {
+    
+                            quantidade: item.Quantidade,
+                            precoUnitario: item.PrecoUnitario,
+                            saleRelation: {
+                                connect: {
+                                    id: venda.id
+                                }
+                            },
+                            productRelation: {
+                                connect: {
+                                    id: item.Id
+                                }
+                            }
+    
+                        }
+    
+                    });
+    
+
+                 }));
+
+            }));
+
+        }
+
+        // jsonVendas.forEach(async (element:any) => {
+
+        //     const venda = await prisma.sales.create({
+
+        //        data: {
+
+        //            data: new Date(element.Data),
+        //            clientRelation: {
+        //                connect: {
+        //                    id: element.IdCliente
+        //                }
+        //            },
+        //        }
+
+        //     });
+
+        //     element.Itens.forEach(async (item:any) => {
+
+        //         await prisma.itemsSold.create({
+
+        //             data: {
+
+        //                 quantidade: item.Quantidade,
+        //                 precoUnitario: item.PrecoUnitario,
+        //                 saleRelation: {
+        //                     connect: {
+        //                         id: venda.id
+        //                     }
+        //                 },
+        //                 productRelation: {
+        //                     connect: {
+        //                         id: item.Id
+        //                     }
+        //                 }
+
+        //             }
+
+        //         });
 
 
-            })
+        //     })
 
-        })
+        // })
+
+        await saveData();
 
         const sales = await prisma.sales.findMany();
 
