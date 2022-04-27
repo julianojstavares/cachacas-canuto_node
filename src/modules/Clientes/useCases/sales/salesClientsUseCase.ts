65d6b5cd4aeff3a9ec788979444f28078f3449b7
Juliano Tavares <julianojstavares@outlook.com>
2022-04-27 05:35:22 -0300

    Adding sales clients use case

---
+++ src/modules/Clientes/useCases/sales/salesClientsUseCase.ts
@@ -0,0 +1,137 @@
+import { prisma } from "../../../../database/prismaClient";
+
+interface ISalesClients {
+
+    dataInicial?: string;
+    dataFinal?: string;
+    classificados?: number;
+
+}
+
+interface IRelatorio {
+
+    nomeCliente?: string;
+    totalVendas?: number;
+    produtos?: IMaisVendidos[];
+}
+
+interface IMaisVendidos {
+    nomeProduto?: string;
+    somaQuantidadeVendidaProduto?: number;
+    somaValorVendidoProduto?: number;
+}
+
+export class SalesClientsUseCase {
+
+    async execute({ dataInicial, dataFinal, classificados }: ISalesClients) {
+
+
+        let start = new Date(dataInicial as string);
+        let end = new Date(dataFinal as string);
+        let dateClause = { gte: start, lte: end };
+
+        let sales : IRelatorio[] = [];
+        let mostSold : IMaisVendidos[] = [];
+
+        const clientes = await prisma.clients.findMany({
+            select:{
+                nome: true,
+            }
+        })
+
+        const produtos = await prisma.products.findMany({
+            select: {
+                nome: true,
+            }
+        })
+
+        for (let i = 0; i < clientes.length; i++) {
+
+            const cliente = clientes[i];
+
+            const totalVendas = await prisma.sales.count({
+                where: {
+                    clientRelation: {
+                        nome: cliente.nome,
+                    },
+                    data: dateClause,
+                }
+            })
+
+            for (let j = 0; j < produtos.length; j++) {
+
+                const quantidade = await prisma.itemsSold.aggregate({
+
+                    where: {
+                        saleRelation: {
+                            clientRelation: {
+                                nome: clientes[i].nome
+                            },
+                            data: dateClause,
+                        },
+                        productRelation: {
+                            nome: produtos[j].nome
+                        }
+                    },
+                    _sum: {
+                        quantidade: true
+                    },
+
+                })
+
+                const totalVendido = await prisma.itemsSold.aggregate({
+
+                    where: {
+                        saleRelation: {
+                            clientRelation: {
+                                nome: clientes[i].nome
+                            },
+                            data: dateClause,
+                        },
+                        productRelation: {
+                            nome: produtos[j].nome
+                        }
+                    },
+                    _sum: {
+                        precoUnitario: true
+                    },
+
+                })
+
+                mostSold.push({
+                    nomeProduto: produtos[j].nome,
+                    somaQuantidadeVendidaProduto: Number(quantidade._sum.quantidade),
+                    somaValorVendidoProduto: Number(totalVendido._sum.precoUnitario)
+                });
+
+            }
+
+            mostSold.sort((a,b) => b.somaQuantidadeVendidaProduto! - a.somaQuantidadeVendidaProduto!);
+
+            let ranking;
+
+            if (classificados) {
+                ranking = mostSold.slice(0, classificados);
+            }
+
+            if(classificados)
+            
+            sales.push({
+
+                nomeCliente: clientes[i].nome,
+                totalVendas: Number(totalVendas),
+                produtos: ranking,
+            })
+
+            mostSold = [];
+
+          }
+
+          sales.sort((a,b) => b.totalVendas! - a.totalVendas!);
+
+        return sales;        
+
+    }    
+
+}
+
